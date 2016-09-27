b0057411f9923c91b238e7e8382e494e4be92664
Vladimir Kofman <vladimir.kofman@intel.com>
2016-09-27 12:58:09 +0300

    Swift 3

--- ALThreeCircleSpinner/ALThreeCircleSpinner.swift
+++ ALThreeCircleSpinner/ALThreeCircleSpinner.swift
@@ -8,21 +8,21 @@
 
 import UIKit
 
-public class ALThreeCircleSpinner: UIView {
+open class ALThreeCircleSpinner: UIView {
     
-    private var stopped: Bool = false
+    fileprivate var stopped: Bool = false
     
     /// true if the loader should be hidden when it is not animating, default = true
-    public var hidesWhenStopped: Bool = true
+    open var hidesWhenStopped: Bool = true
     
     /// The color of the loader view
-    override public var tintColor: UIColor! {
+    override open var tintColor: UIColor! {
         didSet {
             if let _ = tintColor {
                 for sublayer in layer.sublayers! {
                     let _sublayer = sublayer
                     
-                    _sublayer.backgroundColor = tintColor.CGColor
+                    _sublayer.backgroundColor = tintColor.cgColor
                 }
             }
         }
@@ -43,10 +43,10 @@ public class ALThreeCircleSpinner: UIView {
     /**
     * Start animating the loader view
     */
-    public func startAnimating() {
+    open func startAnimating() {
         if !isAnimating() {
             stopped = false
-            hidden = false
+            isHidden = false
             resumeLayers()
         }
     }
@@ -55,10 +55,10 @@ public class ALThreeCircleSpinner: UIView {
     * Stop animating the loader view
     * if hidesWhenStopped = true then the loader will be hidden as well
     */
-    public func stopAnimating() {
+    open func stopAnimating() {
         if isAnimating() {
             if hidesWhenStopped {
-                hidden = true
+                isHidden = true
             }
             stopped = true
             pauseLayers()
@@ -68,13 +68,13 @@ public class ALThreeCircleSpinner: UIView {
     /**
     * returns true if the loader is animating
     */
-    public func isAnimating() -> Bool {
+    open func isAnimating() -> Bool {
         return !stopped
     }
     
-    private func resumeLayers() {
+    fileprivate func resumeLayers() {
         let pausedTime = layer.timeOffset
-        let timeSincePause = layer.convertTime(CACurrentMediaTime(), fromLayer: nil) - pausedTime
+        let timeSincePause = layer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
         
         layer.speed = 1.0;
         layer.timeOffset = 0.0;
@@ -82,14 +82,14 @@ public class ALThreeCircleSpinner: UIView {
         layer.beginTime = timeSincePause;
     }
     
-    private func pauseLayers() {
-        let pausedTime = layer.convertTime(CACurrentMediaTime(), fromLayer: nil)
+    fileprivate func pauseLayers() {
+        let pausedTime = layer.convertTime(CACurrentMediaTime(), from: nil)
         
         layer.speed = 0.0;
         layer.timeOffset = pausedTime;
     }
     
-    private func setupAnimation(layer: CALayer, size: CGSize, color: UIColor) {
+    fileprivate func setupAnimation(_ layer: CALayer, size: CGSize, color: UIColor) {
         let beginTime = CACurrentMediaTime();
         
         let offset: CGFloat = size.width / 8;
@@ -101,14 +101,14 @@ public class ALThreeCircleSpinner: UIView {
             let circleY = size.height / 2 - circleSize/2
             
             let circle = CALayer();
-            circle.frame = CGRectMake(circleX, circleY, circleSize, circleSize);
-            circle.backgroundColor = color.CGColor;
-            circle.anchorPoint = CGPointMake(0.5, 0.5);
-            circle.cornerRadius = CGRectGetHeight(circle.bounds) * 0.5;
+            circle.frame = CGRect(x: circleX, y: circleY, width: circleSize, height: circleSize);
+            circle.backgroundColor = color.cgColor;
+            circle.anchorPoint = CGPoint(x: 0.5, y: 0.5);
+            circle.cornerRadius = circle.bounds.height * 0.5;
             circle.transform = CATransform3DMakeScale(0.0, 0.0, 0.0);
             
             let anim = CAKeyframeAnimation(keyPath: "transform");
-            anim.removedOnCompletion = false;
+            anim.isRemovedOnCompletion = false;
             anim.repeatCount = Float.infinity;
             anim.duration = 1.5;
             anim.beginTime = beginTime + CFTimeInterval(0.25 * CGFloat(i));
@@ -121,13 +121,13 @@ public class ALThreeCircleSpinner: UIView {
             ];
             
             anim.values = [
-                NSValue(CATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)),
-                NSValue(CATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)),
-                NSValue(CATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0))
+                NSValue(caTransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)),
+                NSValue(caTransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)),
+                NSValue(caTransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0))
             ];
             
             layer.addSublayer(circle)
-            circle.addAnimation(anim, forKey: "anime")
+            circle.add(anim, forKey: "anime")
         }
     }
 }
